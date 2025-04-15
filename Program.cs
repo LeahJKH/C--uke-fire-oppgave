2da5b1b138fc2a92ea32325f83d5de370a2250f9
LeahJKH <leahjb06.kodehode@gmail.com>
2025-04-15 11:43:08 +0200

    search for name aswell

--- Program.cs
+++ Program.cs
@@ -5,32 +5,32 @@
         static void Main(string[] args)
         {
             var digimons = CsvReader.ReadDigis();
-            var digiDataSet = new DigiDataset {DigiMons = digimons};
+            var digiDataSet = new DigiDataset { DigiMons = digimons };
 
             var controller = new Controller(digiDataSet);
             controller.Run();
 
             Runnify();
-            void Runnify () 
+            void Runnify()
             {
                 Console.WriteLine("do you wish too check another? y/n");
-                string? yorn = Console.ReadLine();
-                switch(yorn?.ToLower())
+                var yorn = Console.ReadLine();
+                switch (yorn?.ToLower())
                 {
-                    case "y": 
+                    case "y":
                         controller.Run();
                         Runnify();
                         break;
                     case "n":
                         Console.WriteLine("thank you for using :3");
                         break;
-                    default: 
+                    default:
                         Runnify();
                         break;
                 }
             }
 
         }
-        
+
     }
 }
--- controllers/Digicontroller.cs
+++ controllers/Digicontroller.cs
@@ -4,22 +4,49 @@ using System.Linq;
 public class Controller
 {
     private DigiDataset _dataset;
-   public Controller(DigiDataset dataset)
-{
-    _dataset = dataset;
-}
+    public Controller(DigiDataset dataset)
+    {
+        _dataset = dataset;
+    }
 
     public void Run()
     {
-        Console.Write("velg Digimon id:");
-        int answear = Convert.ToInt32(Console.ReadLine());
-
-var result = _dataset.DigiMons.Where(s => s.Number == answear);
-
-        foreach (var digi in result)
+        Console.WriteLine("what do you want too search for?\n1.Name\n2.id");
+        var searchquery = Console.ReadLine();
+        switch (searchquery)
         {
-            Console.WriteLine($"{digi.Digimon}");
+            case "1":
+                SearchName();
+                break;
+            case "2":
+                searchID();
+                break;
+            default:
+                Console.WriteLine("use the numbers that was shown");
+                break;
         }
+        void SearchName() 
+        {
+           Console.Write("velg Digimon name: ");
+        string? answear = Console.ReadLine();
+        var result = _dataset.DigiMons.Where(s => s.Digimon  == answear); 
+         foreach (var digi in result)
+        {
+            Console.WriteLine($"Here is your digimon:\nName of digimon: {digi.Digimon}.\nType: {digi.Type}.\nAttribute: {digi.attribute}\nStage: {digi.Stage}\n");
+        }
+        }
+        void searchID () 
+        {
+        Console.Write("velg Digimon id: ");
+        int answear = Convert.ToInt32(Console.ReadLine());
+        var result = _dataset.DigiMons.Where(s => s.Number == answear);
+         foreach (var digi in result)
+        {
+            Console.WriteLine($"\nName of digimon: {digi.Digimon}.\nType: {digi.Type}.\nAttribute: {digi.attribute}");
+        }
+        }
+
+       
     }
 
 
